a0b4a41d92b3c833ca38ecd21089fb4551a87b75
SambitOKCL <154324801+SambitOKCL@users.noreply.github.com>
2024-08-07 15:19:31 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Title of the app
-st.title("Simple Input Display App")
+st.title("Simple Input Display App for OCAC")
 
 # Text input widget
 user_input = st.text_input("Enter some text:")
@@ -9,6 +9,6 @@ user_input = st.text_input("Enter some text:")
 # Button to display the input
 if st.button("Show Input"):
     # Display the input text
-    st.write("You entered:", user_input)
+    st.write("You entered text is:", user_input)
 
 # Run the app using: streamlit run app.py
